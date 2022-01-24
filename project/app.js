28c8a470b8f832e828125957c2d20cab6c77801a
SeokJu <asdfg7123@ajou.ac.kr>
2022-01-24 14:33:56 +0900

    html로 기본 페이지 구성 및 css기초 잡기

--- project/app.js
+++ project/app.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const mysql = require('mysql');
+//const db = require("../db.js");
 const path = require("path");
 
 const app = express();
--- project/public/js/mainpage.js
+++ project/public/js/mainpage.js
@@ -4,7 +4,7 @@ const body = document.createElement("div");
 export default function main_Page_Button(){
     const body = mainPage();
     body.className = "mainPage";
-    body.addEventListener("click", () => console.log("hello you clicked mainPage"));
+    //body.addEventListener("click", () => console.log("hello you clicked mainPage"));
     root.appendChild(body);
 }
 
